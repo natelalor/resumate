977f3d403be9eff398f59f10a5084606e733d57b
Tate Osborne <89999710+tateosborne@users.noreply.github.com>
2024-08-08 18:39:03 -0400

    initial menu bar setup and basic stylign

---
+++ src/components/MenuBar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { func } from "prop-types";
+import React, {useState} from "react";
+
+const MenuBar = () => {
+    const [tab, changeTab] = useState('Edison')
+
+
+    function changeMenu() {
+        console.log(99);
+    }
+
+    return (
+        <>
+            <div className="absolute top-12 left-1/2 transform -translate-x-1/2 flex flex-row justify-center rounded-xl shadow-custom-heavy">
+                <p onClick={changeMenu} className="p-4 rounded-xl">Browse</p>
+                <p className="p-4 rounded-xl">Edison</p>
+                <p className="p-4 rounded-xl">Profile</p>
+            </div>
+        </>
+
+    )
+};
+
+export default MenuBar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  mode: 'jit',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +9,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+        boxShadow: {
+            'custom-light': '0 4px 6px rgba(0, 0, 0, 0.1)',
+            'custom-heavy': '0 0px 30px rgba(0, 0, 0, 0.15)',
+        }
     },
   },
   plugins: [],
