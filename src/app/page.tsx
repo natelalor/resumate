95cfc05f50e6ba05b40aa5379b380b9f785f09c7
Nate Lalor <natelalor@gmail.com>
2024-08-14 12:53:54 -0400

    adding nav active animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,38 +10,44 @@ export default function Login() {
   return (
     <main className="flex flex-col items-center">
         {/* title container */}
-        <div className="text-6xl text-theme-orange p-20">
+        <div className="text-8xl text-theme-orange p-20">
           <p>Resumate</p>
         </div>
         {/* body container */}
-        <div className="shadow-lg rounded-2xl">
-          {/* login container */}
-          <div className="">
-            {/* nav container */}
-            <div className="flex justify-center shadow-lg">
-              <div onClick={() => setActiveTab('login')} className={`cursor-pointer px-4 py-2 rounded-lg tab-transition ${
-              activeTab === 'login' ? 'text-white bg-theme-orange tab-active' : 'text-text-color'
-            }`}>
-                <p>Login</p>
-              </div>
-              <div onClick={() => setActiveTab('signup')} className={`cursor-pointer px-4 py-2 rounded-lg tab-transition ${
-              activeTab === 'signup' ? 'text-white bg-theme-orange tab-active' : 'text-text-color'
-            }`}>
-                <p>Signup</p>
-              </div>
+        <div className="inline-flex flex-col items-center shadow-lg rounded-2xl">
+          {/* nav container */}
+          <div className="inline-flex justify-center items-center shadow-lg rounded-lg">
+            <div
+              onClick={() => setActiveTab('login')}
+              className={`cursor-pointer px-4 py-2 rounded-lg tab-transition ${
+                activeTab === 'login'
+                  ? 'text-white bg-theme-orange tab-active z-20 transform scale-110'
+                  : 'text-text-color z-10'
+              }`}
+            >
+              <p>Login</p>
             </div>
-            
-            {/* add an action here when you know where this data will be going: https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit */}
-            <form className="flex flex-col space-y-4 py-10 px-5"> 
-              <label htmlFor="email">Email:</label>
-              <input type="text" id="email" name="email" placeholder="me@me.com" ></input>
-              <label htmlFor="password">Password:</label>
-              <input type="password" id="password" name="password" placeholder="•••••••••••"></input>
-              <input type="submit" value="Submit"></input>
-            </form>
-
-
+            <div
+              onClick={() => setActiveTab('signup')}
+              className={`cursor-pointer px-4 py-2 rounded-lg tab-transition ${
+                activeTab === 'signup'
+                  ? 'text-white bg-theme-orange tab-active z-20 transform scale-110'
+                  : 'text-text-color z-10'
+              }`}
+            >
+              <p>Signup</p>
+            </div>
           </div>
+
+          
+          {/* add an action here when you know where this data will be going: https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit */}
+          <form className="flex flex-col space-y-4 py-10 px-5"> 
+            <label htmlFor="email">Email:</label>
+            <input type="text" id="email" name="email" placeholder="me@me.com" ></input>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" placeholder="•••••••••••"></input>
+            <input type="submit" value="Submit"></input>
+          </form>
         </div>
     </main>
   );
