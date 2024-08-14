8d1d0ba1c741faf07294d2f88995de2c062d5c97
Nate Lalor <natelalor@gmail.com>
2024-08-14 09:36:58 -0400

    implementing tailwind requirements. converting current styling to tailwind.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <body>
-        <div className="title">
+      <body className="flex flex-col items-center">
+        <div className="text-6xl text-theme-orange">
           <p>Resumate</p>
         </div>
         <div className="body">
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      'theme-orange': 'rgb(225, 85, 6)',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
